793f1748ea86ec87a21b7237c62f3ae8a93eac88
HaribabuParella <92096609+HaribabuParella@users.noreply.github.com>
2024-10-21 17:19:26 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,28 @@
+provider "google" {
+  project     = "hari-cloud-first-project"
+  region      = "us-central1"
+}
+
+resource "google_compute_instance" "default" {
+  name         = "instance-vpc-automation"
+  machine_type = "e2-medium"
+  zone        = "us-central1-c"
+
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      labels = {
+        my_label = "value"
+      }
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+}
